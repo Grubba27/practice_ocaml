3896f0d5b4aaaadc01cd2bc3f426aa8455a88cc9
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-06-26 22:17:18 -0300

    added identation

--- bin/exercims/hamming.ml
+++ bin/exercims/hamming.ml
@@ -1,6 +1,9 @@
 (*type nucleotide = A | C | G | T*)
 
-let make_list l = String.to_seq l |> List.of_seq |> List.map (String.make 1);;
+let make_list l = String.to_seq l
+  |> List.of_seq
+  |> List.map (String.make 1);;
+
 let eq_counter acc l1 l2  =
   if String.compare l1 l2 == 0
     then acc
